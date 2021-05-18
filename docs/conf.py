a5744fc13a32a96bb45435066149c6e30e42f94a
fcurella <flavio.curella@gmail.com>
2021-05-18 10:11:25 -0500

    fix version in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'Frank Wiles'
 # built documents.
 #
 # The short X.Y version.
-version = '1.3.1'
+version = '1.4.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
