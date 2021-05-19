a4fecd4d79b000c899eda76d63c3a0445e5ca6ab
fcurella <flavio.curella@gmail.com>
2021-05-19 11:21:08 -0500

    Bump version: 2.0.0 → 2.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'Frank Wiles'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0.0'
+version = '2.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ f.close()
 
 setup(
     name="django-test-plus",
-    version="2.0.0",
+    version="2.0.1",
     description="django-test-plus provides useful additions to Django's default TestCase",
     long_description=readme,
     long_description_content_type="text/markdown",
