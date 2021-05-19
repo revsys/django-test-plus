a841432caf93518d48f1a0184cd5cddcf09e42e0
fcurella <flavio.curella@gmail.com>
2021-05-19 11:37:34 -0500

    Bump version: 2.0.1 → 2.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ f.close()
 
 setup(
     name="django-test-plus",
-    version="2.0.1",
+    version="2.1.0",
     description="django-test-plus provides useful additions to Django's default TestCase",
     long_description=readme,
     long_description_content_type="text/markdown",
