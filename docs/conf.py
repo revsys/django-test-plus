4807765941c5530d47605f4fa1e97e234ed9232b
fcurella <flavio.curella@gmail.com>
2021-05-19 14:11:12 -0500

    Bump version: 2.1.0 → 2.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'Frank Wiles'
 # built documents.
 #
 # The short X.Y version.
-version = '2.1.0'
+version = '2.2.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
