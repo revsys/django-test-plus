db312be30716bbc33c5c85fa274694199f26e4f3
Frank Wiles <frank@revsys.com>
2023-06-27 13:42:51 -0500

    Bump version: 2.2.1 → 2.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'Frank Wiles'
 # built documents.
 #
 # The short X.Y version.
-version = '2.2.1'
+version = '2.2.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
