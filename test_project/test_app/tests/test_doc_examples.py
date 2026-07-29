3bdfdd9736209d6bfc921ddefc5633eaeca01cf6
Jeff Triplett <jeff.triplett@gmail.com>
2026-07-29 12:31:12 -0500

    Apply ruff format

--- test_project/test_app/tests/test_doc_examples.py
+++ test_project/test_app/tests/test_doc_examples.py
@@ -2,29 +2,35 @@
 low_query_counts.rst. If you change those examples, change these too.
 """
 
+
 def test_the_view(tp):
-    tp.get('view-context-with')
+    tp.get("view-context-with")
     tp.response_200()
-    tp.assertInContext('testvalue')
+    tp.assertInContext("testvalue")
+
 
 def test_auth(tp, db):
-    user = tp.make_user('u1')
-    tp.assertLoginRequired('view-needs-login')
+    user = tp.make_user("u1")
+    tp.assertLoginRequired("view-needs-login")
     with tp.login(user):
-        tp.get_check_200('view-needs-login')
+        tp.get_check_200("view-needs-login")
+
 
 def test_auth_methods(tp):
-    tp.assertLoginRequired('view-needs-login')
-    tp.assertLoginRequired('view-needs-login', method='post')
+    tp.assertLoginRequired("view-needs-login")
+    tp.assertLoginRequired("view-needs-login", method="post")
+
 
 def test_restrictions(tp, db):
-    user1 = tp.make_user('u1')
+    user1 = tp.make_user("u1")
     with tp.login(user1):
-        tp.get('view-needs-login')
+        tp.get("view-needs-login")
+
 
 def test_better_than_nothing(tp, db):
-    tp.assertGoodView('view-200')
+    tp.assertGoodView("view-200")
+
 
 def test_query_ctx(tp, db):
     with tp.assertNumQueriesLessThan(7):
-        tp.get('view-200')
+        tp.get("view-200")
